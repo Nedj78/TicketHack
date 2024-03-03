1658d7f06facdefa3d734a71649f22066f23617e
Nedj <ikhlef.nedjmeddine@gmail.com>
2024-03-03 02:33:06 +0100

    Update

--- Frontend/all-tickets-script.js
+++ Frontend/all-tickets-script.js
@@ -11,37 +11,35 @@ function getAllCities() {
                     const arrivalCity = ticket.arrival[0].toUpperCase() + ticket.arrival.slice(1);
                     const departureCity = ticket.departure[0].toUpperCase() + ticket.departure.slice(1);
                     const dateTicket = new Date(ticket.date);
+                    
+                    const hourTicket = String(dateTicket.getHours()).padStart(2, '0');
+                    const minuteTicket = String(dateTicket.getMinutes()).padStart(2, '0');
+                    let dayTicket = String(dateTicket.getDate()).padStart(2, '0');
+                    let monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
 
-                    if (dateTicket) {
-                        const hourTicket = String(dateTicket.getHours()).padStart(2, '0');
-                        const minuteTicket = String(dateTicket.getMinutes()).padStart(2, '0');
-                        let dayTicket = String(dateTicket.getDate()).padStart(2, '0');
-                        let monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
-
-                        if (dateTicket.getMonth() + 1 > 9) {
-                            monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
-                        }
-                        
-                        if (dateTicket.getDate() > 0 && dateTicket.getDate() < 10) {
-                            dayTicket = String(dateTicket.getDate()).padStart(2, '0');
-                        }
-
-                        const yearTicket = dateTicket.getFullYear();
-                        const formattedDate = `${dayTicket}/${monthTicket}/${yearTicket} at ${hourTicket}:${minuteTicket}`;
-
-                        document.querySelector('.content-dashboard').innerHTML += `
-                        <div class="trip-card">
-                            <div class="trip">
-                                <div>
-                                    <span class="city-departure">${departureCity}</span>
-                                    <span>&nbsp;&rsaquo;&nbsp;</span>
-                                    <span class="city-arrival">${arrivalCity}</span>
-                                </div>
-                                    <span class="date">${formattedDate}</span>
-                                    <span class="amount"><span class="currency">${ticket.price}&nbsp;€</span></span>
-                            </div>
-                        </div>`;
+                    if (dateTicket.getMonth() + 1 > 9) {
+                        monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
                     }
+                    
+                    if (dateTicket.getDate() > 0 && dateTicket.getDate() < 10) {
+                        dayTicket = String(dateTicket.getDate()).padStart(2, '0');
+                    }
+
+                    const yearTicket = dateTicket.getFullYear();
+                    const formattedDate = `${dayTicket}/${monthTicket}/${yearTicket} at ${hourTicket}:${minuteTicket}`;
+
+                    document.querySelector('.content-dashboard').innerHTML += `
+                    <div class="trip-card">
+                        <div class="trip">
+                            <div>
+                                <span class="city-departure">${departureCity}</span>
+                                <span>&nbsp;&rsaquo;&nbsp;</span>
+                                <span class="city-arrival">${arrivalCity}</span>
+                            </div>
+                                <span class="date">${formattedDate}</span>
+                                <span class="amount"><span class="currency">${ticket.price}&nbsp;€</span></span>
+                        </div>
+                    </div>`;
                 });
             }
         })
