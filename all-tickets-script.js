6b45bc168e6636efa5cbc8bbce376d385863588d
Nedj <ikhlef.nedjmeddine@gmail.com>
2024-03-25 12:29:14 +0100

    update

--- all-tickets-script.js
+++ all-tickets-script.js
@@ -1,41 +1,72 @@
-document.addEventListener('DOMContentLoaded', function () {
-    getAllCities();
+// Fonction exécutée lorsque le contenu de la page est chargé
+document.addEventListener('DOMContentLoaded', async function () {
+    showLoadingIcon();   // Affiche l'icône de chargement
+    await getAllTicketsWithDelay();    // Attend la récupération de tous les billets avec un délai de 3 secondes
+    hideLoadingIcon();    // Cache l'icône de chargement après la récupération des billets
 });
 
-function getAllCities() {
-    fetch('http://localhost:3000/tickets')
-        .then(response => response.json())
-        .then(data => { 
-            if (data.tickets) {
-                data.tickets.forEach(ticket => {
-                    const arrivalCity = ticket.arrival[0].toUpperCase() + ticket.arrival.slice(1);
-                    const departureCity = ticket.departure[0].toUpperCase() + ticket.departure.slice(1);
-                    const dateTicket = new Date(ticket.date);
-                    
-                    let hourTicket = String(dateTicket.getHours()) % 12;
-                    hourTicket = hourTicket === 0 ? 12 : hourTicket;
+// Fonction pour afficher l'icône de chargement
+function showLoadingIcon() {
+    const loadingIcon = document.querySelector('#loading_icon');
+    loadingIcon.style.display = 'block';
+}
 
-                    const minuteTicket = String(dateTicket.getMinutes()).padStart(2, '0');
-                    let dayTicket = String(dateTicket.getDate()).padStart(2, '0');
-                    let monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
-                    const yearTicket = dateTicket.getFullYear();
+// Fonction pour cacher l'icône de chargement
+function hideLoadingIcon() {
+    const loadingIcon = document.querySelector('#loading_icon');
+    loadingIcon.style.display = 'none';
+}
 
-                    if (dateTicket.getMonth() + 1 > 9) {
-                        monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
-                    }
-                    
-                    if (dateTicket.getDate() > 0 && dateTicket.getDate() < 10) {
-                        dayTicket = String(dateTicket.getDate()).padStart(2, '0');
-                    }
+// Fonction pour cacher tous les billets
+function hideTickets() {
+    const tripCards = document.querySelectorAll('.trip-card');
+    tripCards.forEach(tripCard => {
+        tripCard.style.display = 'none';
+    });
+}
 
-                    let meridiem = 'am';
-                    if (dateTicket.getHours() >= 12) {
-                        meridiem = 'pm';
-                    } 
+// Fonction pour récupérer tous les billets avec un délai de 3 secondes
+async function getAllTicketsWithDelay() {
+    await new Promise(resolve => setTimeout(resolve, 3000));    
+    hideTickets();   // Après le délai, cacher tous les billets avant de récupérer les données
 
-                    const formattedDate = `${dayTicket}/${monthTicket}/${yearTicket} at ${hourTicket}:${minuteTicket} ${meridiem}`;
+    try {
+        await getAllTickets();   // Récupérer tous les billets de train
+    } catch (error) {
+        throw error;   // En cas d'erreur, rejeter la promesse
+    }
+}
 
-                    document.querySelector('.content-dashboard').innerHTML += `
+// Fonction asynchrone pour récupérer toutes les villes de départ et d'arrivée
+async function getAllTickets() {
+    try {
+        const fetchResponse = await fetch('http://localhost:3000/tickets');    // Envoie une requête au serveur pour récupérer tous les billets disponibles
+
+        const data = await fetchResponse.json();    // Récupère les données JSON de la réponse
+
+        // Vérifie si des billets sont disponibles dans les données récupérées
+        if (data.tickets) {
+            data.tickets.forEach(ticket => {     // Parcourt tous les billets et les affiche sur la page
+                // Formatage de la date et de l'heure du billet
+                const arrivalCity = ticket.arrival[0].toUpperCase() + ticket.arrival.slice(1);
+                const departureCity = ticket.departure[0].toUpperCase() + ticket.departure.slice(1);
+                const dateTicket = new Date(ticket.date);
+
+                let hourTicket = String(dateTicket.getHours()) % 12;
+                hourTicket = hourTicket === 0 ? 12 : hourTicket;
+                const minuteTicket = String(dateTicket.getMinutes()).padStart(2, '0');
+                let dayTicket = String(dateTicket.getDate()).padStart(2, '0');
+                let monthTicket = String(dateTicket.getMonth() + 1).padStart(2, '0');
+                const yearTicket = dateTicket.getFullYear();
+
+                let meridiem = 'am';
+                if (dateTicket.getHours() >= 12) {
+                    meridiem = 'pm';
+                }
+                const formattedDate = `${dayTicket}/${monthTicket}/${yearTicket} at ${hourTicket}:${minuteTicket} ${meridiem}`;
+
+                // Ajoute le billet au contenu de la page
+                document.querySelector('.content-dashboard').innerHTML += `
                     <div class="trip-card">
                         <div class="trip">
                             <div>
@@ -43,14 +74,15 @@ function getAllCities() {
                                 <span>&nbsp;&rsaquo;&nbsp;</span>
                                 <span class="city-arrival">${arrivalCity}</span>
                             </div>
-                                <span class="date">${formattedDate}</span>
-                                <span class="amount"><span class="currency">${ticket.price}&nbsp;€</span></span>
+                            <span class="date">${formattedDate}</span>
+                            <span class="amount"><span class="currency">${ticket.price}&nbsp;€</span></span>
                         </div>
                     </div>`;
-                });
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            });
+        }
+    } catch (error) {
+        // Gestion des erreurs lors de la récupération des billets
+        console.error('Error:', error);
+        throw error;
+    }
 }
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const ticketsList = []; 
+// Un tableau pour stocker les billets affichés sur la page
+const ticketsList = [];
 
+// Déclaration du bouton de recherche
+const searchButton = document.querySelector('.btn-search');
+
+// Fonction pour créer et afficher un nouveau billet sur la page
 function createTicket(ticket) {
+    // Formatage des données pour l'affichage frontend du billet
     const arrivalCity = ticket.arrival[0].toUpperCase() + ticket.arrival.slice(1);
     const departureCity = ticket.departure[0].toUpperCase() + ticket.departure.slice(1);
     const dateTicket = new Date(ticket.date);
@@ -9,21 +15,25 @@ function createTicket(ticket) {
     const minuteTicket = String(dateTicket.getMinutes()).padStart(2, '0');
     let formattedDate = `${hourTicket}:${minuteTicket}`;
 
+    // Ajout du suffixe 'am' ou 'pm' en fonction de l'heure de la journée
     if (dateTicket.getHours() > 0 && dateTicket.getHours() < 11 ) {
         formattedDate = `${hourTicket}:${minuteTicket} am`;
     } else {
         formattedDate = `${hourTicket}:${minuteTicket} pm`;
     }
 
+    // Sélection de l'élément où les billets seront ajoutés
     const tripCard = document.querySelector('#content-right');
 
-     const isTicketListDisplayed = ticketsList.some(ticketsList => {
+    // Vérification si le billet est déjà affiché pour éviter les doublons et cumuls d'affichage de billets
+    const isTicketListDisplayed = ticketsList.some(ticketsList => {
         return ticketsList.arrival === arrivalCity &&
                ticketsList.departure === departureCity &&
                ticketsList.date === formattedDate &&
                ticketsList.price === ticket.price;
     });
 
+    // Si le billet n'est pas déjà affiché, il est ajouté à la page et au tableau
     if (!isTicketListDisplayed) {
         tripCard.innerHTML += `
           <div class="trip-card">
@@ -42,32 +52,32 @@ function createTicket(ticket) {
     }
 }
 
-function fetchTickets() {
-    let errorMessage = document.querySelector('#error-message');
+// Fonction asynchrone pour récupérer les billets depuis le serveur
+async function fetchTickets() {
+    const tripCard = document.querySelector('#content-right');
 
     const departure = document.querySelector('#departure-input').value; 
     const arrival = document.querySelector('#arrival-input').value; 
     const date = document.querySelector('#date-input').value;
-
+    
     let inputFields = document.querySelectorAll('#departure-input, #arrival-input, #date-input');
-
     let optionOne = document.querySelector('#option-one');
-    const tripCard = document.querySelector('#content-right');
-    const searchButton = document.querySelector('.btn-search');
+    let errorMessage = document.querySelector('#error-message');
 
+    // Nettoyage de l'affichage précédent et du tableau de billets
     tripCard.innerHTML = '';
-
     ticketsList.length = 0;
 
+    // Désactivation du bouton de recherche pendant la requête
     searchButton.disabled = true; 
 
+    // Vérification de la saisie utilisateur et affichage d'un message d'erreur si nécessaire
     if (departure === '' || arrival === '' || date === '') {
         errorMessage.textContent = 'All fields are required';
-        
         inputFields.forEach(field => {
             field.style.border = '1.5px solid red';
         });
-
+        // Affichage d'une carte d'affichage par défaut si des champs sont vides
         tripCard.innerHTML = `
         <div id="option-one">
             <div class="default-card">
@@ -79,13 +89,12 @@ function fetchTickets() {
             </div>
         </div>
         `;
-
         searchButton.disabled = false;
         return;
     } else {
+        // Réinitialisation de l'affichage et des styles en cas de saisie correcte des trois champs de formulaire
         tripCard.innerHTML = '';
         errorMessage.textContent = ''; 
-
         inputFields.forEach(field => {
             field.style.border = 'none';
             if (field === document.querySelector('#departure-input')) {
@@ -94,17 +103,17 @@ function fetchTickets() {
         });
     }
 
-    fetch('http://localhost:3000/tickets/search', {
-        method: 'POST',
-         headers: {'Content-Type':'application/json'},
-         body: JSON.stringify({
-            departure, 
-            arrival,
-            date
-        })
-    })
-    .then(data => {
-        if (!data.ok) {
+    try {
+        // Envoi d'une requête au serveur pour récupérer les billets disponibles en lien avec la requête
+        const responseToFetch = await fetch('http://localhost:3000/tickets/search', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ departure, arrival, date })
+        });
+
+        // Gestion de la réponse du serveur
+        if (!responseToFetch.ok) {
+            // Affichage d'un message si aucun trajet n'est trouvé
             tripCard.innerHTML = '';
             tripCard.innerHTML += `
                 <div class="notrip-card">
@@ -113,27 +122,29 @@ function fetchTickets() {
                         <div class="green-line"></div>
                         <p class="msg-notrip-card">No trip found.</p>
                     </div>
-                </div>`;            
-            throw new Error('Request failed!');
+                </div>`;
+            throw new Error('Request failed!')
         }
-        return data.json();
-    })
-    .then(data => {
-        tripCard.innerHTML = ''; 
+
+        // Récupération des données JSON de la réponse
+        const data = await responseToFetch.json();
+
+        // Réinitialisation de l'affichage initial et affichage des billets récupérés sur la page
+        tripCard.innerHTML = '';
         data.tickets.forEach(ticket => {
             createTicket(ticket);
         });
-    })
-    .catch(error => {
+    } catch (error) {
+        // Gestion des erreurs lors de la récupération des billets
         console.error('Error fetching tickets:', error);
-    })
-    .finally(() => {
-        searchButton.disabled = false; 
-    });
-
-    document.querySelectorAll('#departure-input, #arrival-input, #date-input').forEach(input => {
-        input.value = '';
-    });
+    } finally {
+        // Réactivation du bouton de recherche et nettoyage des champs de saisie
+        searchButton.disabled = false;
+        document.querySelectorAll('#departure-input, #arrival-input, #date-input').forEach(input => {
+            input.value = '';
+        });
+    }
 }
 
-document.querySelector('.btn-search').addEventListener('click', fetchTickets);
+// Ecouteur d'événements pour le clic sur le bouton de recherche de billets
+searchButton.addEventListener('click', fetchTickets);
